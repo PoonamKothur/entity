ae50882cd1bb5e904cd009d1f9f5c937200b0156
Poonam <poonam@codeblock.co.in>
2019-11-15 13:33:43 +0530

    changes n get entity

--- handlers/getentitybyid.js
+++ handlers/getentitybyid.js
@@ -25,8 +25,9 @@ class GetEntitybyId extends BaseHandler {
 
             //check for cuid and euid from route
             if (event && 'pathParameters' in event && event.pathParameters && 'cuid' in event.pathParameters && event.pathParameters.cuid) {
-
+                    console.log("in cuid check");
                 if (event && 'pathParameters' in event && event.pathParameters && 'euid' in event.pathParameters && event.pathParameters.euid) {
+                    console.log("in euid check");
                     let euid = event.pathParameters.euid;
                     let cuid = event.pathParameters.cuid;
                     let res = await this.getEntityByEuid(cuid,euid);
@@ -36,9 +37,12 @@ class GetEntitybyId extends BaseHandler {
                     }
                     return responseHandler.callbackRespondWithSimpleMessage(404, "No customer found !");
                 }
+                else {
+                    return responseHandler.callbackRespondWithSimpleMessage(400, "Please provide euid");
+                }
             }
             else {
-                return responseHandler.callbackRespondWithSimpleMessage(400, "Please provide cuid and euid");
+                return responseHandler.callbackRespondWithSimpleMessage(400, "Please provide cuid");
             }
         }
 
